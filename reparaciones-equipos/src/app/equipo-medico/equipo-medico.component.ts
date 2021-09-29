b71fd74c4df577fa16ca0d69fee8f6ddf123d770
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-09-28 20:52:47 -0500

    .-
    -

--- reparaciones-equipos/src/app/equipo-medico/equipo-medico.component.ts
+++ reparaciones-equipos/src/app/equipo-medico/equipo-medico.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EquipoMedicoService } from './equipo-medico.service';
-import { EquiposMedicos } from './equipoMedico'; //IMPORTA ARCHIVO DE CLASE (.TS)
+import { EquipoMedico } from './equipoMedico'; //IMPORTA ARCHIVO DE CLASE (.TS)
 
 @Component({
   selector: 'app-equipo-medico',
@@ -8,7 +8,7 @@ import { EquiposMedicos } from './equipoMedico'; //IMPORTA ARCHIVO DE CLASE (.TS
   styleUrls: ['./equipo-medico.component.css']
 })
 export class EquipoMedicoComponent implements OnInit {
-  equiposMedicos: EquiposMedicos[] = [];
+  equiposMedicos: EquipoMedico[] = [];
   constructor(private EquipoMedicoService: EquipoMedicoService) { }
 
   ngOnInit(): void {
